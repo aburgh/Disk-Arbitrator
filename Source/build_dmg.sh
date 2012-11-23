536dbf26c5c0574d44828018d52873f280a77c64
Aaron Burghardt <aburgh@mac.com>
2012-11-23 16:41:45 -0500

    Remove existing DMG.

--- Source/build_dmg.sh
+++ Source/build_dmg.sh
@@ -14,7 +14,13 @@ SRC_PRODUCT_VERSION=`/usr/libexec/PlistBuddy -c 'Print CFBundleShortVersionStrin
 
 echo "Building DMG for ${SRC_PRODUCT} ${SRC_PRODUCT_VERSION}"
 
+DMG_DST_PATH="${BUILT_PRODUCTS_DIR}/${SRC_PRODUCT}-${SRC_PRODUCT_VERSION}.dmg"
+
 DMG_SRC_DIR="${CONFIGURATION_TEMP_DIR}/${SRC_PRODUCT}-${SRC_PRODUCT_VERSION}"
+
+if [ -e "$DMG_DST_PATH" ] ; then
+	rm -rf "$DMG_DST_PATH" || exit 1
+fi
 
 mkdir -p "${DMG_SRC_DIR}"
 cp -R "${SRC_PRODUCT_PATH}" "${DMG_SRC_DIR}"
@@ -22,6 +28,6 @@ cp "${SRC_PRODUCT_PATH}/Contents/Resources/README.html" "${DMG_SRC_DIR}"
 cp "${SRC_PRODUCT_PATH}/Contents/Resources/Release Notes.html" "${DMG_SRC_DIR}"
 cp "${SRCROOT}/Resources/Disk Arbitrator Agent.plist" "${DMG_SRC_DIR}"
 
-hdiutil create -layout NONE -srcfolder "${DMG_SRC_DIR}" "${BUILT_PRODUCTS_DIR}/${SRC_PRODUCT}-${SRC_PRODUCT_VERSION}.dmg"
+hdiutil create -layout NONE -srcfolder "${DMG_SRC_DIR}" "$DMG_DST_PATH"
 
 rm -r "${DMG_SRC_DIR}"
